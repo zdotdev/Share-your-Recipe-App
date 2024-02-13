5d41f604c2aee1014aa07cfca9448cb64bfab4f0
Zed0825 <www.ragaszedrick25@gmail.com>
2024-02-13 23:21:23 +0800

    fixed error in editing data in admin panel

--- admin/admin.js
+++ admin/admin.js
@@ -1,12 +1,37 @@
-async function editDish(body,   id){
-    const data = await fetch(`http://localhost:3000/dish/editDish/${id}`, {
+async function editDish(body, id){
+    const data = await fetch(`http://localhost:3000/dish/editReceipt/${id}`, {
         method: "PUT",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(body)
     })
-    console.log(data);
+    console.log(await data.json());
 }
 
+async function getAllDish(){
+    try{
+        const data = await fetch("http://localhost:3000/dish")
+        let dish = await data.json()
+        document.getElementById("data").innerHTML +=`
+        <h1>${dish.dishes[0]._id}<h1>
+        `
+        console.log(dish.dishes[0]._id);
+    }
+    catch(err){
+        console.log(err)
+    }
+    
+}
+getAllDish()
+
+document.getElementById("edit-dish-button").addEventListener("click", () => {
+    const dishID = document.getElementById("edit-dish-ID").value
+    const data = {
+        dishName: document.getElementById("edit-dish-name").value,
+        dishIngredients: editDishIngredients.value,
+        dishProcedure: editDishProcedure.value
+    }
+    editDish(data, dishID)
+})
 
 const addDishIngredients = document.getElementById("add-dish-ingredients")
 const addDishProcedure = document.getElementById("add-dish-procedure")
