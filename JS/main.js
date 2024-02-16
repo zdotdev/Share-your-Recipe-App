72e10344fd0142e029f898fa7139627fc5878993
Zed0825 <www.ragaszedrick25@gmail.com>
2024-02-16 22:23:32 +0800

    Updated background image

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,6 @@
 function card(data){
     Object.values(data.dishes).forEach(dish => {
-        document.getElementById("section").innerHTML += `
+        document.getElementById("dish-container").innerHTML += `
         <div class="card">
             <h2 class="dish-name">${dish.dishName}</h2>
             <p class="dish-procedure">${dish.dishProcedure.slice(0, 200)}...</p>
