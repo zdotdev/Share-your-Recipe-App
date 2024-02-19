3e67803bae09b8403d46c01ffa700b27da0835f9
zdotdev <www.ragaszedrick25@gmail.com>
2024-02-19 23:11:19 +0800

    need to debug navbar

--- JS/main.js
+++ JS/main.js
@@ -18,4 +18,68 @@ async function getAllDishes(){
         console.log(err)
     }
 }
-getAllDishes()
+// getAllDishes()
+
+
+// Add an event listener to detect scrolling
+window.addEventListener('scroll', function(event) {
+    const dishContainer = document.getElementById("dish-container")
+
+    // Get the current vertical scrollbar position
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    // Get the total height of the scrollable content
+    const scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+    // Get the height of the viewport
+    const clientHeight = document.documentElement.clientHeight || window.innerHeight;
+    
+    // Calculate the scroll percentage
+    const scrollPercentage = (scrollTop / (scrollHeight - clientHeight)) * 100;
+    console.log(scrollPercentage.toFixed(2))
+
+    const cards = document.querySelectorAll(".card")
+    
+    const navbar = this.document.getElementById("navbar")
+    
+    // if(scrollPercentage.toFixed(2) >= 20.00){
+
+    //     navbar.classList.remove("normal-pos")
+    //     navbar.classList.add("move-up")
+    //     cards.forEach(card => {
+    //         card.classList.add("translate")
+    //     })
+    // }else{
+    //     navbar.classList.remove("move-up")
+    //     navbar.classList.add("normal-pos")
+    // }
+
+    // if(scrollPercentage.toFixed(2) >= 50.00){
+    //     navbar.classList.remove("normal-pos")
+    // }
+    // else{
+    //     navbar.classList.add("normal-pos")
+    // }
+
+    let lastScrollTop = 0;
+        window.addEventListener('scroll', function() {
+            const currentScroll = document.documentElement.scrollTop;
+    
+            console.log(currentScroll);
+            if (currentScroll > lastScrollTop) {
+                navbar.classList.remove("normal-pos")
+                navbar.classList.add("move-up")
+                if(scrollPercentage.toFixed(2) >= 20.00){
+                    cards.forEach(card => {
+                        card.classList.add("translate")
+                    })
+                }
+            }
+            else{
+                navbar.classList.add("normal-pos")
+                navbar.classList.remove("move-up")
+            }
+            
+    
+            lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+        }, false);
+
+});
