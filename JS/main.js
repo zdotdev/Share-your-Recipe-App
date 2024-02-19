8f7b953ec11c84538436a701a25ff64f6e0e9475
zdotdev <www.ragaszedrick25@gmail.com>
2024-02-19 23:58:41 +0800

    api conneted

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,13 @@
 function card(data){
+    document.getElementById("dish-container").innerHTML = ""
     Object.values(data.dishes).forEach(dish => {
         document.getElementById("dish-container").innerHTML += `
         <div class="card">
+            <img src="./public/img/chicken-adobo_008.jpeg" alt="dish-image" class="dish-image">
             <h2 class="dish-name">${dish.dishName}</h2>
+            <h4>Ingredients</h4>
+            <p class="dish-ingredients">${dish.dishIngredients}...</p>
+            <h4>Procedure</h4>
             <p class="dish-procedure">${dish.dishProcedure.slice(0, 200)}...</p>
         </div>`
     });
@@ -18,68 +23,39 @@ async function getAllDishes(){
         console.log(err)
     }
 }
-// getAllDishes()
+getAllDishes()
 
-
-// Add an event listener to detect scrolling
-window.addEventListener('scroll', function(event) {
+window.addEventListener('scroll', function() {
     const dishContainer = document.getElementById("dish-container")
+    if(dishContainer.querySelector(".card")){
+        console.log("true")
+    }else{
+        console.log("false");
+    }
 
-    // Get the current vertical scrollbar position
     const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    // Get the total height of the scrollable content
     const scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
-    // Get the height of the viewport
     const clientHeight = document.documentElement.clientHeight || window.innerHeight;
-    
-    // Calculate the scroll percentage
     const scrollPercentage = (scrollTop / (scrollHeight - clientHeight)) * 100;
-    console.log(scrollPercentage.toFixed(2))
-
     const cards = document.querySelectorAll(".card")
-    
     const navbar = this.document.getElementById("navbar")
-    
-    // if(scrollPercentage.toFixed(2) >= 20.00){
-
-    //     navbar.classList.remove("normal-pos")
-    //     navbar.classList.add("move-up")
-    //     cards.forEach(card => {
-    //         card.classList.add("translate")
-    //     })
-    // }else{
-    //     navbar.classList.remove("move-up")
-    //     navbar.classList.add("normal-pos")
-    // }
-
-    // if(scrollPercentage.toFixed(2) >= 50.00){
-    //     navbar.classList.remove("normal-pos")
-    // }
-    // else{
-    //     navbar.classList.add("normal-pos")
-    // }
-
     let lastScrollTop = document.documentElement.scrollTop;
-        window.addEventListener('scroll', function() {
-            const currentScroll = document.documentElement.scrollTop;
-    
-            console.log(`${currentScroll.toFixed(2)} : ${lastScrollTop.toFixed(2)}`);
-            if (currentScroll > lastScrollTop) {
-                navbar.classList.remove("normal-pos")
-                navbar.classList.add("move-up")
-                if(scrollPercentage.toFixed(2) >= 20.00){
-                    cards.forEach(card => {
-                        card.classList.add("translate")
-                    })
-                }
-            }
-            if(currentScroll < lastScrollTop){
-                navbar.classList.add("normal-pos")
-                navbar.classList.remove("move-up")
-            }
-            
-    
-            lastScrollTop = currentScroll;
-        });
 
+    window.addEventListener('scroll', function() {
+        const currentScroll = document.documentElement.scrollTop;
+        if (currentScroll > lastScrollTop) {
+            navbar.classList.remove("normal-pos")
+            navbar.classList.add("move-up")
+            if(scrollPercentage.toFixed(2) >= 20.00){
+                cards.forEach(card => {
+                    card.classList.add("translate")
+                })
+            }
+        }
+        if(currentScroll < lastScrollTop){
+            navbar.classList.add("normal-pos")
+            navbar.classList.remove("move-up")
+        }
+        lastScrollTop = currentScroll;
+    });
 });
